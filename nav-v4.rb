0081fe4904507ad90f0787ea829f00545d61477e
mikechau <mchau@iwu.edu>
2012-12-02 21:28:03 -0600

    Rewrite of script, created V.0.0.4. Marking of open positions with stock outstanding and better market price logging.

--- nav-v4.rb
+++ nav-v4.rb
@@ -1,22 +1,19 @@
-# Full Nav Calc V.0.0.3
+# Full Nav Calc V.0.0.4
 require 'date'
 
 transactions = [
                 {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 11.00, :qty => 15.00, :acc_for => 'no'},
                 {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 11.00, :qty => 15.00, :acc_for => 'no'},
                 {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-16', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
                 {:date => '2011-04-17', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
-                {:date => '2011-04-17', :action => "BUY", :ticker => "AAPL", :price => 10.00, :qty => 5.00, :acc_for => 'no' }
-                # {:date => '2011-04-19', :action => "SELL", :ticker => "AAPL", :price => 10.00, :qty => 20.00, :acc_for => 'no'},
-                # {:date => '2011-04-22', :action => "SELL", :ticker => "AAPL", :price => 15.00, :qty => 5.00, :acc_for => 'no'},
-                # {:date => '2011-04-22', :action => "SELL", :ticker => "AAPL", :price => 15.00, :qty => 15.00, :acc_for => 'no'},
-                # {:date => '2011-05-01', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 55.00, :acc_for => 'no'},
-                # {:date => '2011-05-02', :action => "BUY", :ticker => "AAPL", :price => 13.00, :qty => 55.00, :acc_for => 'no'},
-                # {:date => '2011-05-04', :action => "SELL", :ticker => "AAPL", :price => 13.00, :qty => 100.00, :acc_for => 'no'},
-                # {:date => '2011-05-04', :action => "SELL", :ticker => "AAPL", :price => 13.00, :qty => 10.00, :acc_for => 'no'},
-                # {:date => '2011-05-19', :action => "BUY", :ticker => "AAPL", :price => 100.00, :qty => 5000.00, :acc_for => 'no'},
-                # {:date => '2011-05-20', :action => "BUY", :ticker => "AAPL", :price => 101.00, :qty => 500.00, :acc_for => 'no'},
-                # {:date => '2011-05-25', :action => "SELL", :ticker => "AAPL", :price => 100.00, :qty => 500.00, :acc_for => 'no'}
+                {:date => '2011-04-17', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-17', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-19', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-20', :action => "BUY", :ticker => "AAPL", :price => 12.00, :qty => 5.00, :acc_for => 'no'},
+                {:date => '2011-04-20', :action => "BUY", :ticker => "GOOG", :price => 12.00, :qty => 5.00, :acc_for => 'no'}
                ]
 
 eod_prices = [
@@ -33,7 +30,8 @@ eod_prices = [
               {:date => '2011-05-03', :ticker => 'AAPL', :eod_price => 22.00},
               {:date => '2011-05-21', :ticker => 'AAPL', :eod_price => 31.00},
               {:date => '2011-06-04', :ticker => 'AAPL', :eod_price => 10.00},
-              {:date => '2011-06-09', :ticker => 'AAPL', :eod_price => 10.00}
+              {:date => '2011-06-09', :ticker => 'AAPL', :eod_price => 10.00},
+              {:date => '2011-06-09', :ticker => 'GOOG', :eod_price => 10.00}
              ]
 
 ###### EMPTY ARRAYS ###### 
@@ -71,9 +69,23 @@ transactions.each_with_index do |trade,idx|
           open_positions[last_position][:qty] += add_previous_qty
         end
     end
+
+    unique_prices = transactions.select {|o| o[:ticker] == trade[:ticker]}.uniq! {|t| t[:price]}
+    if unique_prices != nil
+      unique_prices.each_with_index do |uq, idx|
+        check_positions = open_positions.select {|o| o[:ticker] == trade[:ticker] && o[:price] == uq[:price]}
+        check_pos_count = check_positions.count - 1
+        check_positions.each_with_index do |pos, idx|
+          if idx < check_pos_count
+            pos[:mark] = 'yes'
+          end
+        end
+    end
+    
+    end
   ###############################################
 
-  ###############################################
+ ###############################################
     #check if the ticker exists in the market data table
     if eod_prices.any? {|t| t[:ticker] == trade[:ticker]}
 
@@ -96,78 +108,39 @@ transactions.each_with_index do |trade,idx|
         end_day = eod_prices.select {|t| t[:ticker] == trade[:ticker] && Date.parse(t[:date]) >= Date.parse(trade[:date])}
         
         end_day.each do |history|
-          market_status_array << {:date => history[:date], :desc => 'MRKT', :ticker => history[:ticker], :price => history[:eod_price]}
+          market_status_array << {:date => history[:date], :desc => 'MRKT', :ticker => history[:ticker], :price => history[:eod_price], :index => trade[:date]}
         end
 
       else
-        puts "ERROR: #{trade}"
+        puts "[EOD] - ERROR: #{trade}"
       end
+
     end
   ###############################################
 
-############# SELL CHECK    
-  elsif trade[:action] == 'SELL'
-
-  ###############################################
-
-  most_recent = open_positions.select {|o| o[:ticker] == trade[:ticker]}
-  most_recent = most_recent.reverse
-  most_recent = most_recent.uniq! {|o| o[:price]}
-  most_recent = most_recent.reverse
-
-  puts "Most recent: #{most_recent}"
-
-    if most_recent.any? {|o| o[:ticker] == trade[:ticker] && o[:qty] >= trade[:qty] && o[:date] == trade[:date]}
-      op = most_recent.find {|o| o[:ticker] == trade[:ticker] && o[:qty] >= trade[:qty] && o[:date] == trade[:date]}
-      difference = op[:qty] - trade[:qty]
-      open_positions << {:date => trade[:date], :ticker => trade[:ticker], :price => op[:price], :qty => difference}
-
-    elsif most_recent.any? {|o| o[:ticker] == trade[:ticker] && o[:qty] >= trade[:qty] && Date.parse(o[:date]) < Date.parse(trade[:date])}
-      matching_positions = most_recent.select {|o| o[:ticker] == trade[:ticker] && o[:qty] > 0 && Date.parse(o[:date]) < Date.parse(trade[:date])}
-      puts matching_positions
-      last_index = (matching_positions.count) - 1
-      previous_qty = matching_positions[last_index -1][:qty]
-      new_qty = previous_qty - trade[:qty]
-
-      open_positions << {:date => trade[:date], :ticker => trade[:ticker], :price => matching_positions[last_index][:price], :qty => new_qty, :debug => '1'}
-
-    elsif most_recent.any? {|o| o[:ticker] == trade[:ticker]}
-      op_select = most_recent.select {|o| o[:ticker] == trade[:ticker] && o[:qty] > 0}
-      op_select = op_select.reverse
-      op_select = op_select.uniq! {|o| o[:price]}
-      if op_select == nil
-        op_select = most_recent.select {|o| o[:ticker] == trade[:ticker] && o[:qty] > 0}
-      else
-        op_select = op_select.reverse
-      end      
-
-      qty_sold = trade[:qty]
-      puts qty_sold
-      op_select.each do |op|
-        if qty_sold > 0
-
-          if qty_sold >= op[:qty]
-            remainder = qty_sold - op[:qty]
-            op_decrease = qty_sold - remainder
-            qty_subtracted = op[:qty] - op_decrease
-            open_positions << {:date => trade[:date], :ticker => trade[:ticker], :price => op[:price], :qty => qty_subtracted, :test => '1'}
-            qty_sold = remainder
-          else
-            qty_subtracted = op[:qty] - qty_sold
-            open_positions << {:date => trade[:date], :ticker => trade[:ticker], :price => op[:price], :qty => qty_subtracted, :test => '2'}
-          end
-        end
-      end
-    else
-      puts "ERROR: #{trade}"
-    end
-  ###############################################
   else
-    puts 'ERROR: Action not BUY or SELL!'
+    puts '[BUY/SELL] - ERROR: Action not BUY or SELL!'
   end
+
 end
 
 puts '----OPEN POSITIONS---------'
 puts open_positions
 puts '----TRANSACTIONS---------'
 puts transactions
+puts '----MARKET STATUS--------'
+#puts market_status_array
+
+capture_tickers = transactions.uniq {|t| t[:ticker]}
+market_log = []
+capture_tickers.each do |ticker|
+  symbol = ticker[:ticker]
+  clean_market_status = market_status_array.select {|m| m[:ticker] == symbol}
+  revised_market_array = clean_market_status.uniq {|c| c[:date]}
+
+  revised_market_array.each do |market|
+    market_log << market
+  end
+end
+
+puts market_log
