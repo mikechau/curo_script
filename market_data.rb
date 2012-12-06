ba33cc8fe712d15b4b525909c34461b0dbd2ccca
mikechau <mchau@iwu.edu>
2012-12-05 20:16:55 -0600

    first commit for market_data collector

---
+++ market_data.rb
@@ -0,0 +1,20 @@
+require 'open-uri'
+require 'csv'
+require 'mechanize'
+
+ticker = 'GOOG'
+
+agent = Mechanize.new
+page = agent.get("http://finance.yahoo.com/q/hp?s=#{ticker}+Historical+Prices")
+
+dl_csv = page.link_with(:text => 'Download to Spreadsheet').href
+
+puts dl_csv
+
+def read(url)
+ CSV.new(open(url), :headers => :first_row).each do |line|
+   puts line['Date']
+ end
+end
+
+read(dl_csv)
